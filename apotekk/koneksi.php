1a3ee1e610e32a7a188e23f6879f25c0ea86d9e2
MILA SARI RUSADI <114979954+milaasari@users.noreply.github.com>
2022-11-01 12:14:26 +0700

    delete apotekk directory

--- apotekk/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$kon= mysqli_connect("localhost","root","","apotekk");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
